c2e46857f790ef81761ea1a782bf36a201a81155
Ray Zana <rayzana280@gmail.com>
2024-06-29 23:51:08 -0400

    adding useEffect and using it to get the tasks data

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])
 
   //TODO Figure out how to fetch json data
   // fetch("./src/users.json")
   //   .then(resp => console.log(resp))
   // ;
+  useEffect(()=>{
+  fetch("http://localhost:3000/tasks")
+  .then(response => response.json())
+  .then(data => setTasks(data))
+  }, [])
 
+  console.log(tasks)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Task To Do</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
